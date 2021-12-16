f8b0b64ce73f6f47106e7b0cd3350b6581cbcb9c
赵家庆 <zhaojiaqing@yxuer.com>
2021-12-16 17:17:26 +0800

    fix 去除opencv依赖

--- setup.py
+++ setup.py
@@ -15,5 +15,5 @@ setup(
     ],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['numpy', 'requests', 'opencv-python', 'tqdm']
+    install_requires=['numpy', 'requests', 'tqdm']
 )
